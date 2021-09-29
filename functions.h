597d83ba7c23f88e6812354a7de76b5805779b5a
scott-mikus <35946339+scott-mikus@users.noreply.github.com>
2021-09-29 12:14:10 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,14 +1,16 @@
 const int PowerPin = 2;
-int pres = 0;
+const int setPoint = 35;
+int pressure = 0;
 int i = 0;
+int readings = 20;
 String lamp = "Off";
 
 void pressRead() {
-  for (int i = 0; i <= 14; i++) {
-    pres = analogRead(A6);
-    pres = map(pres, 100, 1024, 0, 100);
+  for (int i = 0; i <= readings; i++) {
+    pressure = analogRead(A6);
+    pressure = map(pressure, 100, 1024, 0, 100);
     Serial.print("Pressure: ");
-    Serial.print(pres);
+    Serial.print(pressure);
     Serial.println(" psi");
     Serial.print("UVC Lamp ");
     Serial.println(lamp);
@@ -16,9 +18,9 @@ void pressRead() {
   }
 }
 void uvc() {
-  if (pres <= 35) {
+  if (pressure <= setPoint) {
     Serial.print("Pressure: ");
-    Serial.print(pres);
+    Serial.print(pressure);
     Serial.println(" psi");
     lamp = "On";
     Serial.print("UVC Lamp ");
@@ -28,7 +30,7 @@ void uvc() {
 
   else {
     Serial.print("Pressure: ");
-    Serial.print(pres);
+    Serial.print(pressure);
     Serial.println(" psi");
     lamp = "Off";
     Serial.print("UVC Lamp ");
